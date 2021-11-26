234ce8b27d2be6030d252164a9df6d2b7ab883d2
Thiago Bastos <thiagosbastos@live.com>
2021-11-26 19:43:40 -0300

    Added neovim and nodejs

--- devconfig.sh
+++ devconfig.sh
@@ -7,10 +7,11 @@ APT_CMD=$(which apt)
 # Languages and libraries
 if [[ ! -z $DNF_CMD ]]; then
     echo "[DNF BASED INSTALL]";
-    dnf groupinstall "Development Tools" "Development Libraries"   
+    dnf groupinstall "Development Tools" "Development Libraries"
+    dnf install clang-tools-extra nodejs neovim -y   
 elif [[ ! -z $APT_CMD ]]; then
     echo "[APT BASED INSTALL"];
-    apt install build-essential git cmake clang clangd-12
+    apt install build-essential git cmake clang clangd-12 nodejs neovim -y
 else
     echo "[ERROR] Package manager not supported.";
     exit 1;
